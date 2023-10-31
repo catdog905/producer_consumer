a76351cbe5bd3b29ee27aabec1d64955908256ae
Yaroslav <y.kivaev@gmail.com>
2023-10-31 22:47:09 +0800

    WIth print_buffer

--- ProducerConsumer/producer.cpp
+++ ProducerConsumer/producer.cpp
@@ -12,6 +12,14 @@ void signal(int sem_id) {
     semop(sem_id, &v, 1);
 }
 
+void print_buffer(SharedData* shared_data) {
+  std::cout << "Current buffer state: ";
+  for (int i = 0; i < shared_data->count; i++) {
+    std::cout << shared_data->buffer[i] << " ";
+  }
+}
+
+
 int main() {
     // Get shared memory
     int shm_id = shmget(SHM_KEY, sizeof(SharedData), 0666);
@@ -29,8 +37,10 @@ int main() {
         int index = shared_data->count % BUFFER_SIZE;
         shared_data->buffer[index] = i;
         shared_data->count++;
-        std::cout << "Produced: " << i << std::endl;
-
+        std::cout << "Produced: " << i << "; ";
+        print_buffer(shared_data);
+        std::cout << std::endl;
+      
         signal(sem_mutex);
         signal(sem_full);
 
